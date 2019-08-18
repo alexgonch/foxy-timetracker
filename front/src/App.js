76f44e4f5347b2115da1d1aba9b172245a686ad6
Alex Goncharenko <darkstar481@gmail.com>
2019-08-17 21:07:54 -0600

    Landing Page with free Fox logo

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import blueGrey from '@material-ui/core/colors/blueGrey';
+import orange from '@material-ui/core/colors/indigo';
+
+import NoSsr from '@material-ui/core/NoSsr';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider  } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+import LandingPage from 'containers/LandingPage/LandingPage';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: blueGrey[500],
+            main: blueGrey[700],
+            dark: blueGrey[900],
+            contrastText: '#fff'
+        },
+        secondary: orange
+    }
+});
 
 function App() {
-    let local;
-
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <NoSsr>
+            <CssBaseline />
+            <ThemeProvider theme={theme}>
+                <LandingPage />
+            </ThemeProvider>
+        </NoSsr>
     );
 }
 
