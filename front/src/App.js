7c65fe8350c866822248cfe21497efeae51b87f6
Alex Goncharenko <darkstar481@gmail.com>
2019-08-28 19:35:33 -0600

    TODO clarified

--- front/src/App.js
+++ front/src/App.js
@@ -59,7 +59,7 @@ const darkTheme = createMuiTheme({
 // IDEA: global search bar
 // IDEA: multi-lang support
 
-// TODO: create Timer Context
+// TODO: implement Timer Context
 // TODO: implement universal loader and connect to all Firebase actions
 // TODO: implement Auth-agnostic routes under navigation (ToS, Privacy, etc)
 function App() {
